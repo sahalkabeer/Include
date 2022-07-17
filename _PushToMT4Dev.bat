18850794c29c7bcc4399244fb73cbfbdb74edc92
sahalkabeer <sahalkabeer007@gmail.com>
2022-07-18 02:20:34 +0530

    Update _PushToMT4Dev.bat

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -5,7 +5,7 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 setlocal enabledelayedexpansion
 
 set SOURCE_DIR="%PATH_DSS_Repo%\Include"
-set DEST_DIR="%PATH_T2_I%"
+set DEST_DIR="G:\LazyTrade\Trading Terminals\Terminal 4\MQL4\Include"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mqh
 
